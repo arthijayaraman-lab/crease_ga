57f0a1c6b0d183267db1917012205843d0d27081
zijiewu3 <zijiewu@udel.edu>
2021-09-03 06:27:07 -0400

    added sphinx rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+import sphinx_rtd_theme
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -28,7 +29,7 @@ author = 'Zijie Wu, Arthi Jayaraman'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ["m2r2",'sphinx.ext.autosectionlabel','sphinx.ext.autodoc',
-              'numpydoc']
+              'numpydoc','sphinx-rtd-theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,7 +45,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
