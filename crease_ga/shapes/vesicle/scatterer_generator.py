fixed omega calculation

crease_ga/shapes/vesicle/scatterer_generator.py
@@ -46,10 +46,12 @@ def LPOmega(qrange, nAin, nAout, nB, r):                # qvalues number_of_B nu
     omegaarrt=np.zeros((1,len(qrange)))                 # initiating array
     
     omegaarr=np.zeros((1,len(qrange)))              # initiating array
-    rur=r[0,:,:]                                 # selects      
+    rur=r[0,:,:]# selects
+    rur=rur.transpose()
     for i in range(Ntot-1):                         # loops through index and all further indexes to prevent double counting 
-        all_disp = rur[:,i]-rur[:,(i+1):]
-        rij = np.sqrt(np.sum(np.square(all_disp),axis=0)
+        all_disp = rur[i,:]-rur[(i+1):,:]
+        rij = np.sqrt(np.sum(np.square(all_disp),axis=1))
+        rij = rij.transpose()
         rs = rij[:,np.newaxis]                      # reshapes array for consistency
         Q = qrange[np.newaxis,:]                    # reshapes array for consistency
         vals = ne.evaluate("sin(Q*rs)/(Q*rs)")      # ne is efficient at calculations