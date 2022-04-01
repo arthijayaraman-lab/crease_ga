568993d408575ed80f01717a818a7a54d63a4eb2
Zijie Wu <zijiewu@r0login1.localdomain.hpc.udel.edu>
2022-04-01 14:12:38 -0400

    shapes: varying_sd and varying_sd_disp

--- crease_ga/shapes/vesicle_varying_sd_disp/scatterer_generator.py
+++ crease_ga/shapes/vesicle_varying_sd_disp/scatterer_generator.py
@@ -69,7 +69,8 @@ def LPOmega(qrange, r, sigmabead, sld_Ain, sld_B, sld_Aout):                # qv
 
                 vals = ne.evaluate("sum(vals, axis=0)")   # adds together scatterer contributions for each q value
                 #vals = vals.flatten()
-                omegaarr += fi**2*(2*vals+1)      # 1 accounts for the guarenteed overlap of same bead  # 2* accounts for double counting avoided to reduce computational expense by looping for all other pairs
+                if vals.size != 0:
+                    omegaarr += fi**2*(2*vals+1)      # 1 accounts for the guarenteed overlap of same bead  # 2* accounts for double counting avoided to reduce computational expense by looping for all other pairs
             for rj in range(ri+1,len(r)):
                 rsec = r[rj]
                 all_disp = rur[i,:]-rsec
@@ -84,7 +85,8 @@ def LPOmega(qrange, r, sigmabead, sld_Ain, sld_B, sld_Aout):                # qv
                         vals[val[0],val[1]]=1
                 vals = ne.evaluate("sum((vals), axis=0)")   # adds together scatterer contributions for each q value
                 fj = sff*sld[rj]
-                omegaarr += fi*fj*2*vals
+                if vals.size != 0:
+                    omegaarr += fi*fj*2*vals
             
 
     return omegaarr
@@ -114,18 +116,21 @@ def visualize(r, Rcore, dR_Ain, dR_B, dR_Aout, sigmabead):
     
            
 
-def genLP(R_core, dR_Ain, dR_B, dR_Aout, scat_density):  
-        # core radius, inner A layer thickness, B layer thickness, outer A layer thickness, 
-        # bead diameter, # of inner A beads, # of outer A beads, # of B beads
+def genLP(R_core, dR_Ain, dR_B, dR_Aout, scat_density, sB, sAin):  
 
         r = [[],[],[]]
+        n_total = int(4/3*np.pi*((R_core+dR_Ain+dR_B+dR_Aout)**3-R_core**3)*scat_density)
+        nB = int(n_total*sB)
+        v_Ain = 4/3*np.pi*((R_core+dR_Ain)**3-R_core**3)
+        v_Aout = 4/3*np.pi*((R_core+dR_Ain+dR_B+dR_Aout)**3-(R_core+dR_Ain+dR_B)**3)
+        scat_Aout = (n_total-nB)/(v_Aout+v_Ain*sAin)
+        nAout = int(scat_Aout*v_Aout)
+        nAin = n_total-nB-nAout
+
         ### Create configuration for each replicate with dispersity ###
        
-        nAin = int(4/3*np.pi*((R_core+dR_Ain)**3-R_core**3)*scat_density)
         r[0] = gen_layer(R_core,R_core+dR_Ain,nAin)
-        nB = int(4/3*np.pi*((R_core+dR_Ain+dR_B)**3-(R_core+dR_Ain)**3)*scat_density)
         r[1] = gen_layer(R_core+dR_Ain,R_core+dR_Ain+dR_B,nB)
-        nAout = int(4/3*np.pi*((R_core+dR_Ain+dR_B+dR_Aout)**3-(R_core+dR_Ain+dR_B)**3)*scat_density)
         r[2] = gen_layer(R_core+dR_Ain+dR_B,R_core+dR_Ain+dR_B+dR_Aout,nAout)
         
         return r
@@ -138,16 +143,18 @@ class scatterer_generator:
 
     Notes
     -----
-    **The following 7 shape-specific descriptors are to be specified by user (see
+    **The following 3 shape-specific descriptors are to be specified by user (see
     *Attributes*) as 
     a list, in the precise order as listed, while calling `Model.load_shape`
     to load this shape:**
 
 
-    eta_B:
-        scatterer density. Default: 0.5
+    scat_density:
+        overall scatterer density. Default: 0.5
     sigmabead:
         scatterer diameter. Default: 1
+    sB:
+        molar fraction of the B type chemistry. Default: 0.5
 
 
     **The following 7 parameters are to be predicted, in the precise order
@@ -161,20 +168,17 @@ class scatterer_generator:
         R_Ain/(R_total-R_core). Default [min,max]: [0.01,0.99]
     f_Aout:
         R_Aout/(R_total-R_core-R_Ain). Default [min,max]: [0.01,0.99]
-    sld_Ain:
-        SLD of Ain layer. 
-        Default [min,max]: [-1,1]
-    sld_B:
-        SLD of B layer. 
-        Default [min,max]: [-1,1]
-    sld_Aout:
-        SLD of Aout layer. 
-        Default [min,max]: [-1,1]
+    sAin:
+        Scatterer density_in/scatterer_density out. 
+        Default [min,max]: [0.4,1]
+    pd_core:
+	assuming gaussian distribution for Rcore, sigma/mu of gaussian distribution
+        Default [min,max]: [0.1,0.5]
 
     -log10(bg):
         Negative log10 of background intensity. 
         E.g. an background intensity of 0.001 leads to this value being 3.
-        Default [min,max]:  [0.1,4]
+        Default [min,max]:  [1,6]
     
     See also
     --------
@@ -182,16 +186,18 @@ class scatterer_generator:
     '''
     
     def __init__(self,
-                 shape_params = [0.5,1],
-                minvalu = (20, 0.1, 0.01, 0.01, 0, 0, 0, 1),
-                maxvalu = (3000, 0.95, 0.99, 0.99, 1, 1, 1, 7)):
+                 shape_params = [2e-3,1,0.5],
+                minvalu = (20, 0.1, 0.01, 0.01, 0.4, 0.1, 1),
+                maxvalu = (3000, 0.95, 0.99, 0.99, 1, 0.5, 6)):
         scat_density = shape_params[0]
         sigmabead = shape_params[1]
-        self._numvars = 8
+        sB = shape_params[2]
+        self._numvars = 7
         self.minvalu = minvalu
         self.maxvalu = maxvalu
         self.scat_density = scat_density    ## scatterer density
         self.sigmabead = sigmabead    ## scatterer density
+        self.sB = sB
     
     @property
     def numvars(self):
@@ -229,19 +235,21 @@ class scatterer_generator:
         f_core=param[1]
         f_Ain=param[2]
         f_Aout=param[3]
-        sld_Ain=param[4]     # split of type A scatterer 
-        sld_B=param[5]     # split of type A scatterer 
-        sld_Aout=param[6]   # variation in Rcore, dispersity
+        sAin=param[4]     # split of type A scatterer 
         #print(Rcore, dR_Ain, dR_B, dR_Aout, sAin)
-        Background=10**(-param[7])
+        pd = param[5]
+        Background=10**(-param[6])
 
         R_core = R_total*f_core
+        sigma = R_core*pd
         dR_Ain = R_total*(1-f_core)*f_Ain
         dR_Aout = f_Aout*(R_total-R_core-dR_Ain)
         dR_B = R_total-R_core-dR_Ain-dR_Aout
         
-        r = genLP(R_core, dR_Ain, dR_B, dR_Aout, scat_density) 
-        IQid = LPOmega(qrange, r, sigmabead, sld_Ain, sld_B, sld_Aout)
+        IQid = np.zeros(len(qrange))
+        for delta in np.arange(-1.5,1.6,0.5):
+            r = genLP(R_core+delta*sigma, dR_Ain, dR_B, dR_Aout, scat_density, self.sB, sAin) 
+            IQid += LPOmega(qrange, r, sigmabead, 1, 1, 1)
         maxIQ=np.max(IQid)                                  
         IQid=np.true_divide(IQid,maxIQ)                    # normalizes the I(q) to have its maximum = 1
         IQid+=Background                                   # add background
